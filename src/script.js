31d0f0aba89252f1e03b4846924b851aaf387874
Renaud Chrétien <82329478+rchretien@users.noreply.github.com>
2023-12-27 19:21:41 +0000

    chore: added blue circle around hamburger.

--- src/script.js
+++ src/script.js
@@ -1,8 +1,19 @@
 function toggleMenu() {
-    var navbar = document.getElementById("myNavbar");
-    if (navbar.className.indexOf("open") == -1) {
-      navbar.className += " open";
-    } else {
-      navbar.className = navbar.className.replace(" open", "");
-    }
+  var sidebar = document.getElementsByClassName("w3-sidebar")[0];
+  var overlay = document.getElementById("overlay");
+  var hamburgerIcon = document.getElementById("hamburgerIcon");
+  var crossIcon = document.getElementById("crossIcon");
+
+  // Toggle sidebar
+  if (sidebar.classList.contains("open")) {
+      sidebar.classList.remove("open");
+      overlay.classList.remove("open");
+      hamburgerIcon.style.display = "block";
+      crossIcon.style.display = "none";
+  } else {
+      sidebar.classList.add("open");
+      overlay.classList.add("open");
+      hamburgerIcon.style.display = "none";
+      crossIcon.style.display = "block";
   }
+}
