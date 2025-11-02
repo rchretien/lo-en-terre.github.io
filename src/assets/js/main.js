7859ac4d58175967aeb71169481c4703e141fcd4
Renaud Chretien <renaud.chretien@cluepoints.com>
2025-11-02 12:39:19 +0100

    chore: fixed codebase issues

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -151,42 +151,6 @@
   }
 
   /**
-   * Skills animation
-   */
-  let skilsContent = select(".skills-content");
-  if (skilsContent) {
-    new Waypoint({
-      element: skilsContent,
-      offset: "80%",
-      handler: function (direction) {
-        let progress = select(".progress .progress-bar", true);
-        progress.forEach((el) => {
-          el.style.width = el.getAttribute("aria-valuenow") + "%";
-        });
-      },
-    });
-  }
-
-  /**
-   * Portfolio details slider
-   */
-  if (document.querySelector(".portfolio-details-slider")) {
-    new Swiper(".portfolio-details-slider", {
-      speed: 400,
-      loop: true,
-      autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        type: "bullets",
-        clickable: true,
-      },
-    });
-  }
-
-  /**
    * Testimonials slider
    */
   new Swiper(".testimonials-slider", {
